6e727fd3f7c5e295dafa01fa24329de39cd89b0b
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-20 21:16:53 +0300

    step-3

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
+const MOCK_TASKS = [
+  { id: 1, title: "Изучить паттерн MVC", isDone: false },
+  { id: 2, title: "Подготовить моковые данные", isDone: true },
+];
+
 // хранение данных, бизнес-логика
 const model = {
-    tasks: [],
-    addTask(){},
-    deleteTask(){},
-    toggleTask(){}
-}
- 
+  tasks: MOCK_TASKS,
+  addTask() {},
+  deleteTask() {},
+  toggleTask() {},
+};
+
 // отображение данных: рендер списка задач, размещение обработчиков событий
 const view = {
-    render(){}
-}
- 
+  renderTasks(tasks) {
+    let list = document.querySelector(".list");
+    const tasksHTML = "";
+    for (let i = 0; i < tasks.length; i++) {
+      const task = tasks[i];
+      tasksHTML += `<li><span>${task.title}</span><button>Удалить</button></li>`;
+      list.innerHTML = tasksHTML;
+    }
+  },
+};
+
 // обработка действий пользователя, обновление модели
 const controller = {
-    addTask(){},
-    deleteTask(){},
-    toggleTask(){}
-}
+  addTask() {},
+  deleteTask() {},
+  toggleTask() {},
+};
