285f2cff384d20394ba04b9a48a1c67f7ce324a4
viktorKorneev <korneevviktor90@yandex.ru>
2024-09-20 22:39:56 +0300

    step-5

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const MOCK_TASKS = [
   { id: 1, title: "Изучить паттерн MVC", isDone: false },
-  { id: 2, title: "Подготовить моковые данные", isDone: true },
+  { id: 2, title: "Подготовить моковые данные", isDone: false },
 ];
 
 // хранение данных, бизнес-логика
 const model = {
-  tasks: MOCK_TASKS,
-  addTask() {},
+  tasks: [],
+  addTask(title) {
+    const newTask = { title: title, isDone: false, id: Math.random() };
+    // то же, что { title: title, isDone: isDone, id: id }
+
+    this.tasks.push(newTask);
+
+    view.renderTasks(model.tasks); // Обновляем представление
+  },
   deleteTask() {},
   toggleTask() {},
 };
@@ -14,19 +21,35 @@ const model = {
 // отображение данных: рендер списка задач, размещение обработчиков событий
 const view = {
   init() {
-    const tasks = controller.getTasks();
-    this.renderTasks(tasks);
+    // 1
+    this.renderTasks(model.tasks);
+    //2
+    // const tasks = controller.getTasks();
+    // this.renderTasks(tasks);
+    const form = document.querySelector(".form");
+    const input = document.querySelector(".input");
+
+    form.addEventListener("submit", function (event) {
+      event.preventDefault(); // Предотвращаем стандартное поведение формы
+      // const title = input.value
+      controller.addTask(input.value); // Вызываем метод addTask контроллера
+
+      input.value = ""; // Очищаем поле ввода
+    });
   },
   renderTasks(tasks) {
     let list = document.querySelector(".list");
     let tasksHTML = "";
+
     for (let i = 0; i < tasks.length; i++) {
       const task = tasks[i];
-      tasksHTML = tasksHTML + `
-      <li>
-        <span>${task.title}</span>
-        <button>Удалить</button>
-      </li>`;
+      tasksHTML =
+        tasksHTML +
+        `
+      <li id="${task.id}" class="${task.isDone ? "done" : ""}">
+          <b class="task-title">${task.title}</b>
+          <button class="delete-button" type="button">Удалить 🗑</button>
+        </li>`;
       list.innerHTML = tasksHTML;
     }
   },
@@ -37,7 +60,9 @@ const controller = {
   getTasks() {
     return model.tasks;
   },
-  addTask() {},
+  addTask(title) {
+    model.addTask(title);
+  },
   deleteTask() {},
   toggleTask() {},
 };
